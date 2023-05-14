4ed3c72d388e4b487a83f10d2763590c4fe454e2
XaaRii <pgambler@seznam.cz>
2023-05-14 20:54:26 +0200

    fixed /queue

--- nodejs/slashcmds/queue.js
+++ nodejs/slashcmds/queue.js
@@ -8,20 +8,19 @@ module.exports = {
 	async execute(interaction, emitter) {
 		const authorID = interaction.user.id;
 		if (authorID !== "303108947261259776" && authorID !== "1027333555556196397") return await interaction.reply("You are not permitted to do this action.");
-
+		await interaction.deferReply()
 		dbQueue.find({}, async function (err, docs) {
 			if (err) return console.log(err);
-			if (docs.length < 1) return await interaction.reply("Queue is empty.");
+			if (docs.length < 1) return await interaction.followUp("Queue is empty.");
 
 			const response = ["**Queue:**"];
-			const queueList = async () => {
-				for (i of docs) {
-					const e = docs[i];
-					const request = await `${i + 1}. <@${e.author}> angle: ${e.angle}`
-					response.push(request);
-				}
-				return await interaction.followUp({ content: response.join("\n") })
-			};
+			var c = 1
+			for (i of docs) {
+				const request = await `${c}. <@${i.author}> angle: ${i.angle}`
+				c++;
+				response.push(request);
+			}
+			return await interaction.followUp({ content: response.join("\n") })
 		})
 	},
 };
